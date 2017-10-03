7baedd20ff8ec986312e3328ea320ed8fc940c17
JudeWong <judewong95@gmail.com>
2017-10-03 18:19:00 +0800

    Add files via upload
    Create homepage and menu bar

--- register.php
+++ register.php
@@ -1,108 +1,218 @@
-<?php
-	session_cache_limiter('private, must-revalidate');
-	session_start();
-?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>E-Market Portal</title>
-<link rel="stylesheet" type="text/css" href="css/mainmenu.css">
-</head>
 
-<body>
-	<header>
-		<h1>Registration</h1>
-	</header>
-	
-	<div>
-		<p><label for="username">Enter your Username</label>
-			<input type="text" name="username" id="username" size="20" />
-		</p>
-	</div>
-	
-	<div>
-		<p><label for="fullname">Enter your Full Name</label>
-			<input type="text" name="fullname" id="fullname" size="20" />
-		</p>
-	</div>
-	
-	<div>
-		<p><label for="dob">Enter your Date of Birth</label>
-			<input type="text" name="dob" id="dob" size="20" />
-		</p>
-	</div>
+  <head>
 
-	<div>
-		<p><label for="gender">Enter your Gender</label>
-			<input type="text" name="gender" id="gender" size="20" />
-		</p>
-	</div>
-	
-	<div>
-		<p><label for="address">Enter your Address</label>
-			<input type="text" name="address" id="address" size="20" />
-		</p>
-	</div>
-	
-	<div>
-		<p><label for="postcode">Enter your PostCode</label>
-			<input type="text" name="postcode" id="postcode" size="20" />
-		</p>
-	</div>
-	
-	<div>
-		<p><label for="city">Enter your City</label>
-			<input type="text" name="City" id="city" size="20" />
-		</p>
-	</div>
-	
-	<div>
-		<p><label for="state">Enter your State</label>
-			<input type="text" name="state" id="state" size="20" />
-		</p>
-	</div>
-	
-	<div>
-		<p><label for="country">Enter your Country</label>
-			<input type="text" name="country" id="country" size="20" />
-		</p>
-	</div>
-	
-	<div>
-		<p><label for="telno">Enter your Contact Number</label>
-			<input type="text" name="telno" id="telno" size="20" />
-		</p>
-	</div>
-	
-	<div>
-		<p><label for="email">Enter your Email Address</label>
-			<input type="text" name="email" id="email" size="20" />
-		</p>
-	</div>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-	<div>
-		<p><label for="nric">Enter your NRIC</label>
-			<input type="text" name="nric" id="nric" size="20" />
-		</p>
-	</div>
-	
-	<div>
-		<p><label for="acctype">Register as</label>
-			<input type="text" name="acctype" id="acctype" size="20" />
-		</p>
-	</div>
+    <title>e-MarketPlace Portal System</title>
 
-	<div>
-		<p><label for="password">Enter your Password</label>
-			<input type="text" name="password" id="password" size="20" />
-		</p>
-	</div>
-	
-	<div>
-		<p><label for="password2">Confirm your Password</label>
-			<input type="text" name="password2" id="password2" size="20" />
-		</p>
-	</div>
-</body>
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/clean-blog.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Deallo</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.html">About</a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link" href="#">Sell on Deallo</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">LogIn</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="register.php">SignUp</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>Registration</h1>
+              <span class="subheading">Create your Deallo Account</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <p>Please fill in all information</p>
+          
+          <form name="sentMessage" id="contactForm" novalidate>
+            <div class="control-group">
+              <div class="form-group floating-label-form-group controls">
+                <label>Enter your Username</label>
+                <input type="text" class="form-control" placeholder="Username" id="name">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+            <div class="control-group">
+              <div class="form-group floating-label-form-group controls">
+                <label>Enter your password</label>
+                <input type="password" class="form-control" placeholder="Password" id="password">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+			<div class="control-group">
+              <div class="form-group floating-label-form-group controls">
+                <label>Enter your password</label>
+                <input type="password" class="form-control" placeholder="Confirm Password" id="c_password">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+			<div class="control-group">
+              <div class="form-group floating-label-form-group controls">
+                <label>Enter your Full Name</label>
+                <input type="text" class="form-control" placeholder="Full Name" id="full_name">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+			<div class="control-group">
+              <div class="form-group floating-label-form-group controls">
+                <label>Enter your NRIC</label>
+                <input type="text" class="form-control" placeholder="IRIC" id="ic">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+			<div class="control-group">
+              <div class="form-group floating-label-form-group controls">
+                <label>Enter your Email</label>
+                <input type="text" class="form-control" placeholder="Email" id="email">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+			<div class="control-group">
+              <div class="form-group floating-label-form-group controls">
+                <label>Enter your Home Address</label>
+                <input type="text" class="form-control" placeholder="Home Address" id="address">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+			<div class="control-group">
+              <div class="form-group floating-label-form-group controls">
+                <label>Enter your Country</label>
+                <input type="text" class="form-control" placeholder="Country" id="country">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+			<div class="control-group">
+              <div class="form-group floating-label-form-group controls">
+                <label>Enter your State</label>
+                <input type="text" class="form-control" placeholder="State" id="state">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+			<div class="control-group">
+              <div class="form-group floating-label-form-group controls">
+                <label>Enter your City</label>
+                <input type="text" class="form-control" placeholder="City" id="city">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+			<div class="control-group">
+              <div class="form-group floating-label-form-group controls">
+                <label>Enter your Phone Number</label>
+                <input type="text" class="form-control" placeholder="Phone Number" id="phone_num">
+                <p class="help-block text-danger"></p>
+              </div>
+            </div>
+            <br>
+			
+            <div id=""></div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-secondary" id="sendMessageButton">Send</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <ul class="list-inline text-center">
+              <li class="list-inline-item">
+                <a href="#">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+            </ul>
+            <p class="copyright text-muted">Copyright &copy; Your Website 2017</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+
+    <!-- Custom scripts for this template -->
+    <script src="js/clean-blog.min.js"></script>
+
+  </body>
+
 </html>
