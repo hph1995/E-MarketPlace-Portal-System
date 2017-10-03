76ee0f03defa185cf84003d90cb3427c1f1c3cb6
hph1995 <100084347@students.swinburne.edu.my>
2017-10-03 22:22:47 +0800

    update tables of database

--- dbEMarketplace.php
+++ dbEMarketplace.php
@@ -4,7 +4,7 @@
 	$Table = array(
 	"CREATE TABLE tbluser(NRIC VARCHAR(15) PRIMARY KEY, username VARCHAR(100), password VARCHAR(32), accType VARCHAR(30), status CHAR(30))",	
 	"CREATE TABLE tblpersonnelaccount(userID VARCHAR(15) PRIMARY KEY, name VARCHAR(80), dob DATE, gender CHAR(1), addr VARCHAR(100), postcode VARCHAR(100), city VARCHAR(100), state VARCHAR(100), country VARCHAR(100), contactNum VARCHAR(15), email VARCHAR(100))",
-	"CREATE TABLE tblproduct(productID INT(5) PRIMARY KEY AUTO_INCREMENT, productName VARCHAR(80), description VARCHAR(100), placeManufacturer VARCHAR(100))",
+	"CREATE TABLE tblproduct(productID INT(5) PRIMARY KEY AUTO_INCREMENT, productName VARCHAR(80), category VARCHAR(50), description VARCHAR(100), placeManufacture VARCHAR(100))",
 	"CREATE TABLE tblsellingprice(priceID INT(15) PRIMARY KEY AUTO_INCREMENT, productID VARCHAR(15), sellingPrice DOUBLE)",
 	"CREATE TABLE tblpayment(ID INT(5) PRIMARY KEY AUTO_INCREMENT, itemName VARCHAR(100), qty INT(10), price DOUBLE, totalAmt DOUBLE, dateCr DATE, receiptNo VARCHAR(15), paymentType VARCHAR(100))");
 	
