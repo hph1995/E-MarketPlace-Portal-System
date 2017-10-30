49b441c435c7a42931e2780cf7f4b12eb23fbf78
hph1995 <100084347@students.swinburne.edu.my>
2017-10-30 20:40:31 +0800

    Fix some error on database file

--- dbEMarketplace.php
+++ dbEMarketplace.php
@@ -10,7 +10,7 @@
 	"CREATE TABLE tblsubscribe(subID INT(5) PRIMARY KEY AUTO_INCREMENT, sellerID VARCHAR(80), customerID VARCHAR(50))",
 	"CREATE TABLE tblsellingprice(priceID INT(15) PRIMARY KEY AUTO_INCREMENT, productID VARCHAR(15), sellerID VARCHAR(15), sellingPrice DOUBLE, status CHAR(30))",
 	"CREATE TABLE tblrating(ratingID INT(15) PRIMARY KEY AUTO_INCREMENT, productID VARCHAR(15), accountID VARCHAR(15), rateNumber DOUBLE)",
-	"CREATE TABLE tblstockcontrol(stockControlID INT(15) PRIMARY KEY AUTO_INCREMENT, productID VARCHAR(15) sellerID VARCHAR(15), quantity INT(15), status CHAR(30))",
+	"CREATE TABLE tblstockcontrol(stockControlID INT(15) PRIMARY KEY AUTO_INCREMENT, productID VARCHAR(15), sellerID VARCHAR(15), quantity INT(15), status CHAR(30))",
 	"CREATE TABLE tblfavourite(favID INT(15) PRIMARY KEY AUTO_INCREMENT, productID VARCHAR(15), accountID VARCHAR(15), status CHAR(30))",
 	"CREATE TABLE tblpayment(ID INT(5) PRIMARY KEY AUTO_INCREMENT, itemName VARCHAR(100), qty INT(10), price DOUBLE, totalAmt DOUBLE, dateCr DATE, receiptNo VARCHAR(15), paymentType VARCHAR(100))");
 	
